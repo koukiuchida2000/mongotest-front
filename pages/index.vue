b51eaba7b7ebf29605d590f089c96602a28b2314
koukiuchida2000 <kouki.heart@gmail.com>
2023-02-15 10:33:45 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <div v-for="d in dat.SampleModel" :key=d.title align="center">
+          <h2>
+            ID:{{d.id}} <br>
+            title:{{d.title}} <br>    
+             comment:{{d.description}} 
+          </h2>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dat: []
+    }
+  },
+  async mounted(){
+    const url = "/get_person/" 
+    const response = await this.$axios.get(url)
+    this.dat = response.data
+  }
+}
+</script>
